2ad6bb0056186980dd278cedacd033e511800430
RoyhanRPL2 <royhanbumi227@gmail.com>
2023-11-05 00:14:35 +0700

    improve website performance #8

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  assetsInclude: /\.(png|jpe?g|gif|svg|css|js|json)$/ ,
   plugins: [react()],
   base: "/"
 })
