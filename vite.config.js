33d38384d3184625653f229cacd33927d09a298b
RoyhanRPL2 <royhanbumi227@gmail.com>
2023-11-05 00:17:50 +0700

    improve website performance

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    assetsInclude: /\.(png|jpe?g|gif|svg|css|js|json)$/, // Ekstensi file yang ingin diatur cache-control-nya
+  },
   plugins: [react()],
-  base: "/"
-})
+  base: "/",
+});
